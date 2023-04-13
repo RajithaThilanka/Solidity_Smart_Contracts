1480fcd49c9faf3c5b9102b3fb6ac98cb4159369
RajithaThilanka <rajithat1998@gmail.com>
2023-04-13 22:21:10 +0530

    update

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,14 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MyContract{
+contract MyContract {
     //State variables
-    uint myUint =1;
-    uint256 myUint2 =1;
+    uint256 myInt = 1;
+    uint public myUint = 1;
+    uint256 public myUint2 = 1;
+    uint8 public myUint3 = 1;
+
+    string public myString = "String variables";
+    bytes32 public myByte32 = "This is 32 byte string";
+  
+    address public myAddress = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+   
+    struct MyStruct {
+        uint256 myUint;
+        string myString;
+    }
     
+    MyStruct public myStruct = MyStruct(1, "hello");
+
+
     //Local variables
     function getValue() public pure returns(uint) {
-       uint value=1;
-       return value;  
+        uint value = 1;
+        return value;  
     }
 }
